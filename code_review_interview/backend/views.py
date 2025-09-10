76fd5d1b0536ab757f13bdd7c04b6feef8eacda7
Nathan Klingensmith <nathan@jellyfish.co>
2025-09-10 14:02:25 -0400

    Add GetPeopleView

--- code_review_interview/backend/views.py
+++ code_review_interview/backend/views.py
@@ -1,6 +1,7 @@
 
 from code_review_interview.backend.models import Team, Person, JiraIssue
 from rest_framework import generics, serializers
+from rest_framework.response import Response
 
 
 class TeamSerializer(serializers.ModelSerializer):
@@ -31,3 +32,41 @@ class GetJiraIssuesView(generics.ListAPIView):
 
     def get_queryset(self):
         return JiraIssue.objects.all().select_related('team')
+    
+
+class PersonSerializer(serializers.ModelSerializer):
+    team = TeamSerializer()
+    class Meta:
+        model = Person
+        fields = [
+            'id',
+            'name',
+            'team',
+        ]
+    
+
+class GetPeopleView(generics.ListAPIView):
+    serializer_class = PersonSerializer
+
+    def get(self):
+        people = Person.objects.all()
+
+        for person in people:
+            issue_count_by_team = {}
+            for issue in person.assigned_issues:
+                team_id = issue.team.id
+                if team_id not in issue_count_by_team:
+                    issue_count_by_team[team_id] = 0
+                issue_count_by_team[team_id] += 1
+
+            max_issue_count = 0
+            allocated_team_id = None
+            for team_id, issue_count in issue_count_by_team.items():
+                if issue_count > max_issue_count:
+                    max_issue_count = issue_count
+                    allocated_team_id = team_id
+
+            person.team = Team.objects.get(id=allocated_team_id)
+
+        serialized_data = self.serializer_class(people, many=True).data
+        return Response(serialized_data)
