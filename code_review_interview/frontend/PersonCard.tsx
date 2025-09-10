788413b17a837e89b6bcdc443d521373658452e2
Nathan Klingensmith <nathan@jellyfish.co>
2025-09-10 14:12:09 -0400

    Add click handler

--- code_review_interview/frontend/PersonCard.tsx
+++ code_review_interview/frontend/PersonCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Person } from './types';
 
@@ -7,8 +8,10 @@ type PersonCardProps = {
 };
 
 export const PersonCard: React.FC<PersonCardProps> = (props) => {
+  const navigate = useNavigate();
+
   return (
-    <div style={{ marginTop: '24px' }}>
+    <div style={{ marginTop: '24px' }} onClick={() => navigate(`/person/${props.person.id}`)}>
       <div>{props.person.name}</div>
       <div>Team: {props.person.team.name}</div>
     </div>
