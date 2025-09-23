38f18db6eeb54b6ab3dc93d3bd16c0684a9390d2
Nathan Klingensmith <nathan@jellyfish.co>
2025-09-23 09:37:21 -0400

    Remove unneeded fields

--- code_review_interview/backend/models.py
+++ code_review_interview/backend/models.py
@@ -3,24 +3,17 @@ from django.db import models
 
 class Team(models.Model):
 
-    id = models.AutoField(primary_key=True)
-
     name = models.CharField(max_length=64)
 
 
 class Person(models.Model):
 
-    id = models.AutoField(primary_key=True)
-
     name = models.CharField(max_length=64)
-    email = models.EmailField(max_length=64, unique=True)
 
 
 class JiraIssue(models.Model):
 
-    id = models.AutoField(primary_key=True)
     jira_id = models.UUIDField(editable=False)
-
     title = models.TextField()
     assignee = models.ForeignKey(Person, related_name='assigned_jira_issues', on_delete=models.CASCADE)
     team = models.ForeignKey(Team, on_delete=models.SET_NULL)
